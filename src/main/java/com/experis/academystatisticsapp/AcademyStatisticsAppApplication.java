0e4d21696c1f2ab971eb73dc2bc4b269a169dc3a
xtrmil <emil.oja@gmail.com>
2021-06-15 09:46:42 +0200

    lillap,xtrmil: initial commit

---
+++ src/main/java/com/experis/academystatisticsapp/AcademyStatisticsAppApplication.java
@@ -0,0 +1,13 @@
+package com.experis.academystatisticsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyStatisticsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyStatisticsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/experis/academystatisticsapp/AcademyStatisticsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.academystatisticsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyStatisticsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
