18948696725cc6dd8902e8134b752b6637406dc7
xtrmil <emil.oja@gmail.com>
2021-06-17 10:13:16 +0200

    lillap,xtrmil Test

--- src/main/java/com/experis/academystatisticsapp/models/User.java
+++ src/main/java/com/experis/academystatisticsapp/models/User.java
@@ -1,7 +1,7 @@
 package com.experis.academystatisticsapp.models;
 
 import lombok.Data; // Lombok, tool for auto generation of getters and setters by using @Data in model class
-
+// TEST
 @Data
 public class User {
     private long id;
