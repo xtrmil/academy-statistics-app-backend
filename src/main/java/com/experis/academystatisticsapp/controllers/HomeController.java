5f5806d468d8bcb26fd7dd12397a660e1b48b607
xtrmil <emil.oja@gmail.com>
2021-09-06 15:59:05 +0200

    xtrmil (SecurityConfig,application.yml,application.properties,pom.xml,HomeController) created SecurityConfig,application.yml,HomeController(for testing), added dependencies

---
+++ src/main/java/com/experis/academystatisticsapp/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.experis.academystatisticsapp.controllers;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping( "home" )
+    public String home( @AuthenticationPrincipal(expression = "claims['name']") String name ) {
+        return String.format( "Hello %s!  welcome to the Security app", name);
+    }
+}
