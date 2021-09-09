12293e38410fda24aba144b749a0cd8d372cfcc7
xtrmil <emil.oja@gmail.com>
2021-09-09 10:11:10 +0200

    xtrmil (AcademyStatisticsAppApplication,AcademyStatisticsAppApplicationTests,HomeController,KickstartOAuth2UserService,KickstatUserInfoResponseClient,pom,SecurityConfig) added replacements classes for OAuth2UserService and UserInfoResponseClients. updated pom.xml,SecurityConfig,AcademyStatisticsAppApplication and its Testclass, updated the test controller HomeController

--- src/main/java/com/experis/academystatisticsapp/controllers/HomeController.java
+++ src/main/java/com/experis/academystatisticsapp/controllers/HomeController.java
@@ -2,25 +2,45 @@ package com.experis.academystatisticsapp.controllers;
 
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HomeController {
 
-    @PreAuthorize("hasRole('ROLE_Users')")
-    @RequestMapping("/")
-    public String helloWorld() {
-        return "Hello Users!";
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @RequestMapping("/claims")
+    String claims() {
+        return "Data: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+    }
+
+    @RequestMapping("/patient/{id}")
+    ModelAndView patient(ModelMap model, HttpServletRequest request, @PathVariable String id) {
+        model.addAttribute("attribute", "forwardWithForwardPrefix");
+
+        String client_id = "";
+        int state = (int )(Math.random() * 1000000 + 1);
+        String resource = "client_id_guid_of_fhir";
+        String base_uri = request.getRequestURL().toString();
+        base_uri = base_uri.substring(0, base_uri.length() - request.getServletPath().length());
+        String redirect_uri = base_uri + "/context/authorized";
+
+        String redirect = String.format("https://login.microsoftonline.com/common/oauth2/authorize?client_id=%s&state=%s&resource=%s&redirect_uri=%s&response_type=code&response_mode=post", client_id, state, resource, redirect_uri);
+        return new ModelAndView("redirect:" + redirect, model);
     }
     @PreAuthorize("hasRole('ROLE_Test')")
-    @RequestMapping("/Test")
+    @RequestMapping("/")
     public String groupOne() {
-        return "Hello Group 1 Users!";
-    }
-    @PreAuthorize("hasRole('ROLE_group2')")
-    @RequestMapping("/Group2")
-    public String groupTwo() {
-        return "Hello Group 2 Users!";
+        return "Hello Test Users!";
     }
 }
